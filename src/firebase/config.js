0082cb46d7c2b75996da82e48fd7440f0617ac52
Robert German <robert.german@reea.net>
2024-09-10 21:51:43 +0300

    Added firebase config

---
+++ src/firebase/config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBjmUxkPmKIVrSyAjLGN6fVwZ4CAgHwPSw",
+  authDomain: "alex-szilagyi-photography-223.firebaseapp.com",
+  projectId: "alex-szilagyi-photography-223",
+  storageBucket: "alex-szilagyi-photography-223.appspot.com",
+  messagingSenderId: "383069288680",
+  appId: "1:383069288680:web:05b64eb1f9321b4486715d",
+};
