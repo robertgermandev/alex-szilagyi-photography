6360268878f8bc243ab9c2cfc51347641e16be7b
Robert German <robert.german@reea.net>
2024-09-10 21:11:11 +0300

    Added routing, mobile nav, socials and others'

--- src/components/Socials.js
+++ src/components/Socials.js
@@ -1,7 +1,29 @@
-import React from 'react';
+import React from "react";
+import { ImFacebook, ImInstagram } from "react-icons/im";
 
 const Socials = () => {
-  return <div>Socials</div>;
+  return (
+    <div className="hidden xl:flex ml-24">
+      <ul className="flex gap-x-4">
+        <li>
+          <a
+            href="https://www.instagram.com/alexszilagyiphotography/"
+            target="_blank"
+          >
+            <ImInstagram />
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.facebook.com/alexszilagyiphotography"
+            target="_blank"
+          >
+            <ImFacebook />
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default Socials;
