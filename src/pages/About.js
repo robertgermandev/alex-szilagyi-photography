92b9f64e5f5fc5fe8111a94fa57b3a9d971ca1b7
Robert German <robert.german@reea.net>
2024-09-11 10:41:05 +0300

    Added animations

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,7 +1,51 @@
 import React from "react";
+import WomanImg from "../img/about/woman.png";
+import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
+import { transition1 } from "../transitions";
 
 const About = () => {
-  return <section className="section bg-yellow-200">About</section>;
+  return (
+    <motion.section
+      initial={{ opacity: 0, y: "100%" }}
+      animate={{ opacity: 1, y: 0 }}
+      exit={{ opacity: 0, y: "100%" }}
+      transition={transition1}
+      className="section"
+    >
+      <div className="container mx-auto h-full relative">
+        <div className="flex flex-col lg:flex-row h-full items-center justify-center gap-x-24 text-center lg:text-left lg:pt-16">
+          <div className="flex-1 max-h-96 lg:max-h-max order-2 lg:order-none overflow-hidden">
+            <img src={WomanImg} />
+          </div>
+          <motion.div
+            initial={{ opacity: 0, y: "-80%" }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: "-80%" }}
+            transition={transition1}
+            className="flex-1 pt-36 pb-14 lg:pt-0 lg:w-auto z-10 flex flex-col justify-center items-center lg:items-start"
+          >
+            <h1 className="h1">About me</h1>
+            <p className="mb-12 max-w-sm">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Architecto a odio obcaecati asperiores aliquam quo! Quos
+              dignissimos repellat, architecto tempora eligendi rerum officiis,
+              veniam voluptates, aliquid commodi earum omnis doloribus!
+              <br />
+              <br />
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Saepe
+              consequuntur aliquam exercitationem accusamus aliquid sit tenetur
+              repellendus in ea cumque perspiciatis aut quasi odio delectus qui
+              officia veniam, repudiandae dolore!
+            </p>
+            <Link to={"/portfolio"} className="btn">
+              View my work
+            </Link>
+          </motion.div>
+        </div>
+      </div>
+    </motion.section>
+  );
 };
 
 export default About;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,27 @@
 import React from "react";
 import WomanImg from "../img/home/woman.png";
 import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
+import { transition1 } from "../transitions";
 
 const Home = () => {
   return (
-    <section className="section">
+    <motion.section
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      transition={transition1}
+      className="section"
+    >
       <div className="container mx-auto h-full relative">
         <div className="flex flex-col justify-center">
-          <div className="w-full pt-36 pb-14 lg:pt-0 lg:pb-0 lg:w-auto z-10 lg:absolute flex flex-col justify-center items-center lg:items-start">
+          <motion.div
+            initial={{ opacity: 0, y: "-50%" }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: "-50%" }}
+            transition={transition1}
+            className="w-full pt-36 pb-14 lg:pt-0 lg:pb-0 lg:w-auto z-10 lg:absolute flex flex-col justify-center items-center lg:items-start"
+          >
             <h1 className="h1">photographer</h1>
             <p className="text-[26px] lg:text-[36px] font-primary mb-4 lg:mb-12">
               Alba Iulia, Romania
@@ -15,11 +29,26 @@ const Home = () => {
             <Link to={"/contact"} className="btn mb-[30px]">
               hire me
             </Link>
+          </motion.div>
+          <div className="flex justify-end max-h-96 lg:max-h-max">
+            <motion.div
+              initial={{ scale: 0 }}
+              animate={{ scale: 1 }}
+              exit={{ scale: 0 }}
+              transition={transition1}
+              className="relative lg:right-30 overflow-hidden"
+            >
+              <motion.img
+                whileHover={{ scale: 1.1 }}
+                transition={transition1}
+                src={WomanImg}
+                alt="photographer-img"
+              />
+            </motion.div>
           </div>
-          <div>image</div>
         </div>
       </div>
-    </section>
+    </motion.section>
   );
 };
 
